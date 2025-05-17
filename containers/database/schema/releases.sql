41db1765b972379c36689f7db50d488b894c6385
Simone <fardella93@gmail.com>
2025-05-17 13:56:29 +0200

    Update releases.sql
    Signed-off-by: Simone <fardella93@gmail.com>

--- containers/database/schema/releases.sql
+++ containers/database/schema/releases.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS releases (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  runtime_version VARCHAR(255) NOT NULL,
+  runtime_version VARCHAR(2048) NOT NULL,
   path VARCHAR(255) NOT NULL,
   timestamp TIMESTAMP NOT NULL,
   commit_hash VARCHAR(255) NOT NULL,
-  commit_message VARCHAR(255) NOT NULL,
+  commit_message VARCHAR(2048) NOT NULL,
   update_id VARCHAR(255)
 );
